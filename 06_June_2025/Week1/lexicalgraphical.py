de906631f520d83be6b48e66137ddcd22ec9289d
Radhika Thakur <radhikathakur1103@gmail.com>
2025-06-05 23:14:06 +0530

    lexicographically-smallest-equivalent-string

---
+++ 06_June_2025/Week1/lexicalgraphical.py
@@ -0,0 +1,24 @@
+class Solution:
+    def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
+        parent = {chr(i): chr(i) for i in range(97, 123)}  # a-z
+
+        def find(c):
+            if parent[c] != c:
+                parent[c] = find(parent[c])
+            return parent[c]
+
+        def union(a, b):
+            rootA = find(a)
+            rootB = find(b)
+            if rootA == rootB:
+                return
+            # always attach the larger to the smaller to keep lex smallest
+            if rootA < rootB:
+                parent[rootB] = rootA
+            else:
+                parent[rootA] = rootB
+
+        for a, b in zip(s1, s2):
+            union(a, b)
+
+        return ''.join(find(c) for c in baseStr)
