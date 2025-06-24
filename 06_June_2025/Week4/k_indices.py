a25c25232faf1fdd1aec4573ad43e889090c8151
Radhika Thakur <radhikathakur1103@gmail.com>
2025-06-24 19:49:02 +0530

    find-all-k-distant-indices-in-an-array

---
+++ 06_June_2025/Week4/k_indices.py
@@ -0,0 +1,8 @@
+index = []
+        n = len(nums)
+        for i in range(n):
+            for j in range(n):
+                if nums[j] == key and abs(i - j) <= k:
+                    index.append(i)
+                    break
+        return index
