35351f3a8d97ea18fa580c6a22f0625aad774de1
Radhika Thakur <radhikathakur1103@gmail.com>
2025-09-01 22:32:12 +0530

    heap

---
+++ 09_September_2025/Week1/01_pass_total.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
+        def gain(p,t):
+            return (t-p)/(t*(t+1))
+        
+        heap =[(-gain(p,t),p,t) for p,t in classes]
+        heapq.heapify(heap)
+
+        for _ in range(extraStudents):
+            g,p,t=heapq.heappop(heap)
+            p+=1
+            t+=1
+            heapq.heappush(heap, (-gain(p,t),p,t))
+        
+        total_ratio=sum(p/t for _,p,t in heap)
+        return total_ratio/len(classes)
+
