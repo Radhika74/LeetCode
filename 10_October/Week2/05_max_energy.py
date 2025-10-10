480129e48d5196dddc28ba09e5bddd6d9d7a0e21
Radhika Thakur <radhikathakur1103@gmail.com>
2025-10-10 23:06:39 +0530

    taking-maximum-energy-from-the-mystic-dungeon

---
+++ 10_October/Week2/05_max_energy.py
@@ -0,0 +1,5 @@
+class Solution:
+    def maximumEnergy(self, energy: List[int], k: int) -> int:
+        for i in range(len(energy) - k - 1, -1, -1):
+            energy[i] += energy[i + k]
+        return max(energy)
