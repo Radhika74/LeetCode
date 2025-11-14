3e3301cd32e59844bef667bd3d98883ee31c0a77
Radhika Thakur <136047186+Radhika74@users.noreply.github.com>
2025-11-14 22:33:56 +0530

    Implement rangeAddQueries for submatrix increments

---
+++ 11_November.py/Week2/05_submatrices_increment.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rangeAddQueries(self, n: int, queries: list[list[int]]) -> list[list[int]]:
+        mat = [[0]*n for _ in range(n)]
+        for r1, c1, r2, c2 in queries:
+            for r in range(r1, r2+1):
+                mat[r][c1] += 1
+                if c2+1 < n:
+                    mat[r][c2+1] -= 1
+        for r in range(n):
+            curr = 0
+            for c in range(n):
+                curr += mat[r][c]
+                mat[r][c] = curr
+        return mat
