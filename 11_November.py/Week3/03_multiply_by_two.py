e9b0a0c477a41e63e5c384391b867ce9c25b209d
Radhika Thakur <136047186+Radhika74@users.noreply.github.com>
2025-11-19 12:08:05 +0530

    Create 03_multiply_by_two.py

---
+++ 11_November.py/Week3/03_multiply_by_two.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findFinalValue(self, nums: List[int], original: int) -> int:
+        flag = True
+        while flag:
+            if original in nums:
+                original = original * 2
+            else:
+                return original
